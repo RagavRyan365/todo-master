6ea900cd035f9aac2d8c8bacecde667d117775da
RagavRyan365 <ragavryan2008@gmail.com>
2026-06-06 12:46:10 +0530

    Fix: small error in Task.jsx

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -3,13 +3,13 @@ import { useState,useEffect } from "react";
 function Task({Title,Completed}){
     const [completed,setcompleted] = useState(Completed)
     async function setTask(){
-        
+
         
     }
 
     return(
         <div id="taskcon">
-            <input type="checkbox" id="Checkbox" defaultChecked={completed} onChange={}/>
+            <input type="checkbox" id="Checkbox" defaultChecked={completed} onChange={setTask}/>
             <p style={{textDecoration:completed?"line-through":"none"}}>{Title}</p>
         </div>
 
